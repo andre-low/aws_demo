a17a38ad89bdb6ce0faf85c5d03d3ad269ba7f4b
Andre Low <andrelowwy@gmail.com>
2021-03-14 23:01:00 +0800

    Modified script to allow for input of paths

--- aws_demo.py
+++ aws_demo.py
@@ -13,8 +13,8 @@ polly = aws.client('polly')
 
 
 # Variables
-s3_bucket = ''
-input_audio_file = ''
+s3_bucket = ''  # enter s3 bucket name here
+input_audio_file = ''  # enter input audio file name here
 input_audio_s3_uri = 's3://' + s3_bucket + '/' + input_audio_file
 transcription_job_name = 'transcription_job_' + token_hex(3)
 output_file_name = 'output.mp3'
@@ -24,26 +24,28 @@ output_file_name = 'output.mp3'
 print('Input audio retrieved from: ' + input_audio_s3_uri)
 
 transcribed_response_init = transcribe.start_medical_transcription_job(
-    MedicalTranscriptionJobName = transcription_job_name,
-    LanguageCode = 'en-US',
-    MediaFormat = 'mp3',
-    Media = {
+    MedicalTranscriptionJobName=transcription_job_name,
+    LanguageCode='en-US',
+    MediaFormat='mp3',
+    Media={
         'MediaFileUri': input_audio_s3_uri
     },
-    OutputBucketName = s3_bucket,
-    Specialty = 'PRIMARYCARE',
-    Type = 'DICTATION'
+    OutputBucketName=s3_bucket,
+    Specialty='PRIMARYCARE',
+    Type='DICTATION'
 )
 
 for remaining in range(30, 0, -1):
     sys.stdout.write('\r')
-    sys.stdout.write('Audio transcription in progress, {:2d} seconds remaining.'.format(remaining))
+    sys.stdout.write(
+        'Audio transcription in progress, {:2d} seconds remaining.'.format(remaining))
     sys.stdout.flush()
     time.sleep(1)
 
 
-# Amazon Transcribe: fetch complete transcription to file and parse JSON
-s3.meta.client.download_file(s3_bucket, 'medical/' + transcription_job_name + '.json', transcription_job_name + '.json')
+# Amazon Transcribe: fetch complete transcription to a temporary file and parse JSON
+s3.meta.client.download_file(
+    s3_bucket, 'medical/' + transcription_job_name + '.json', 'transcription.json')
 
 with open('transcription.json') as f:
     transcription_response = json.load(f)
@@ -55,7 +57,7 @@ print('\rTranscribed text: ' + transcribed_text)
 
 # Amazon Translate
 translated_response = translate.translate_text(
-    Text = transcribed_text,
+    Text=transcribed_text,
     SourceLanguageCode='en',
     TargetLanguageCode='zh'
 )
@@ -67,9 +69,9 @@ print('Translated text: ' + translated_text)
 
 # Amazon Polly
 speech_response = polly.synthesize_speech(
-    OutputFormat = 'mp3',
-    Text = translated_text,
-    VoiceId = 'Zhiyu'
+    OutputFormat='mp3',
+    Text=translated_text,
+    VoiceId='Zhiyu'
 )
 
 file = open(output_file_name, 'wb')
