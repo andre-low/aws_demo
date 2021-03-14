c1323a8b99fad97c19a1eb030d45ca2705fa7a35
Andre Low <andrelowwy@gmail.com>
2021-03-14 22:46:16 +0800

    Modified script to allow for input of paths

--- aws_demo.py
+++ aws_demo.py
@@ -4,16 +4,21 @@ import sys
 import json
 from secrets import token_hex
 
+
 # AWS setup
 s3 = aws.resource('s3')
 transcribe = aws.client('transcribe')
 translate = aws.client('translate')
 polly = aws.client('polly')
 
+
 # Variables
-input_audio_s3_uri = 's3://andrelow.awsdemo.transcribe/aspirin.mp3'
+s3_bucket = ''
+input_audio_file = ''
+input_audio_s3_uri = 's3://' + s3_bucket + '/' + input_audio_file
 transcription_job_name = 'transcription_job_' + token_hex(3)
-output_file_name = 'speech_response.mp3'
+output_file_name = 'output.mp3'
+
 
 # Amazon Transcribe: start transcription job
 print('Input audio retrieved from: ' + input_audio_s3_uri)
@@ -25,7 +30,7 @@ transcribed_response_init = transcribe.start_medical_transcription_job(
     Media = {
         'MediaFileUri': input_audio_s3_uri
     },
-    OutputBucketName = 'andrelow.awsdemo.transcribe',
+    OutputBucketName = s3_bucket,
     Specialty = 'PRIMARYCARE',
     Type = 'DICTATION'
 )
@@ -36,8 +41,9 @@ for remaining in range(30, 0, -1):
     sys.stdout.flush()
     time.sleep(1)
 
+
 # Amazon Transcribe: fetch complete transcription to file and parse JSON
-s3.meta.client.download_file('andrelow.awsdemo.transcribe', 'medical/' + transcription_job_name + '.json', 'transcription.json')
+s3.meta.client.download_file(s3_bucket, 'medical/' + transcription_job_name + '.json', transcription_job_name + '.json')
 
 with open('transcription.json') as f:
     transcription_response = json.load(f)
@@ -45,6 +51,7 @@ with open('transcription.json') as f:
 transcribed_text = transcription_response['results']['transcripts'][0]['transcript']
 
 print('\rTranscribed text: ' + transcribed_text)
+
 
 # Amazon Translate
 translated_response = translate.translate_text(
@@ -56,6 +63,7 @@ translated_response = translate.translate_text(
 translated_text = translated_response['TranslatedText']
 
 print('Translated text: ' + translated_text)
+
 
 # Amazon Polly
 speech_response = polly.synthesize_speech(
